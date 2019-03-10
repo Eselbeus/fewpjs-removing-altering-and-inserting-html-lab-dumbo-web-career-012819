3d2f090e3c3bbaf42c38dcca92b82f2847d91b7a
Seann Branchfield <eselbeus@gmail.com>
2019-03-10 16:56:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 // Write your code here!
+<<<<<<< HEAD
 document.querySelector("main").remove();
 let newHeader = document.createElement('h1');
 document.body.appendChild(newHeader);
 newHeader.id = "victory"
 newHeader.innerText = "Seann is the champion"
+=======
+document.querySelector("main").remove()
+let newHeader = createElement('h1 id="victory")
+document.body.appendChild(newHeader)
+>>>>>>> 02dd7e31d0178ae7197cd6c72c63533a533d3776
